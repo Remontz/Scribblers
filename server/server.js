23f8415a61e8bb1a75b0768bfbc4f32541ecb7ba
Kacy Gilbert <kacygilbert.DEV@gmail.com>
2023-06-08 20:22:28 -0500

    refreshToken Route init && added to server.js

--- server/server.js
+++ server/server.js
@@ -27,6 +27,7 @@ app.use(cookieParser())
 app.use('/api/register', require('./routes/api/register.routes'))
 app.use('/api/authorize', require('./routes/api/authorize.routes'))
 app.use('/api/logout', require('./routes/api/logout.routes'))
+app.use('/api/refreshToken', require('./routes/api/refreshToken.routes'))
 
 app.use(verifyJWT)
 // ROUTES AFTER (verifyJWT)
