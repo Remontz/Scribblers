182961ef3d73ac1b37e4a3ff9540eb2bfbf09d54
Kacy Gilbert <kacygilbert.DEV@gmail.com>
2023-06-11 01:46:08 -0500

    user login & auth with axios

--- client/src/views/Unauthorized.js
+++ client/src/views/Unauthorized.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Unauthorized = () => {
+  const navigate = useNavigate()
+
+  const goBack = () => navigate(-1)
+
   return (
-    <div>Unauthorized</div>
+    <div>
+      <h1>Unauthorized</h1>
+      <br />
+      <p>Access Denied</p>
+      <button onClick={goBack}>Go Back</button>
+    </div>
   )
 }
 
