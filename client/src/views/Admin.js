d2d78b39d4179c271f329a745c0ffdf23a22e283
Kacy Gilbert <kacygilbert.DEV@gmail.com>
2023-06-12 09:59:26 -0500

    login jwt auth access&refresh w/ axios interceptors | created Users component

--- client/src/views/Admin.js
+++ client/src/views/Admin.js
@@ -1,7 +1,16 @@
-import React from "react";
+import { HashLink as Link } from "react-router-hash-link";
+import Users from './Users'
 
 const Admin = () => {
-  return <div>Admin</div>;
+  return (
+    <div>
+      <h1>Admins Page</h1>
+      <br />
+      <Users />
+      <br />
+      <Link to='/login'>Login</Link>
+    </div>
+  )
 };
 
 export default Admin;
